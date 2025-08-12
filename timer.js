ec50eeb935ab3c4aa2c850a6ee8f2870632b5c2a
anusayabuchade <anusayabuchade5601@gmail.com>
2025-08-12 22:38:50 +0530

    change list into anchor tag

--- timer.js
+++ timer.js
@@ -1,13 +1,14 @@
-var countDownDate = new Date("Aug 10, 2025 07:41:01").getTime();
+// timer.js
+var countDownDate = new Date("Aug 31, 2025 23:59:59").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var hours = String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
+    var minutes = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
+    var seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, '0');
 
     document.getElementById("demo").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
